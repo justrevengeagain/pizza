95bbab1190da76910505caa5141e38c13a1dcfab
justrevengeagain <mark_joling@hotmail.nl>
2016-05-09 13:41:06 +0200

    Create test.php

---
+++ azalon/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mark
+ * Date: 3-5-2016
+ * Time: 16:23
+ */
+
+for ($x = 0; $x != 25; $x += 1) {
+    for ($i = 0; $i != 100; $i += 1) {
+        echo '*';
+    }
+    echo '<br>';
+}
